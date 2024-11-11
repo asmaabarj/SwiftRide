d11f7f190ee68296858beeeeb500a00767d805da
asmaabarj <asmaabarj5@gmail.com>
2024-11-11 09:39:28 +0100

    first commit

---
+++ src/main/java/com/example/SwiftRide/SwiftRideApplication.java
@@ -0,0 +1,13 @@
+package com.example.SwiftRide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftRideApplication.class, args);
+	}
+
+}
